a1db331532462b8ee82cb30e4e03a069645a606b
jenniferk2019 <51275153+jenniferk2019@users.noreply.github.com>
2019-07-15 14:56:07 -0400

    final successful js codes

--- js/moment.js
+++ js/moment.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         messagingSenderId: "315244355942",
         appId: "1:315244355942:web:ede7af7a4e24bf04"
       };
-            firebase.initializeApp(config);
+            firebase.initializeApp(firebaseConfig);
             var trainData = firebase.database();
             var trainName = "";
             var destination = "";
@@ -36,19 +36,32 @@ $(document).ready(function() {
                 var sv = snapshot.val();
                 var newRow = $("<tr>");
                 var nameData = $("<td>");
-                nameData.text(sv.name);
+                nameData.text(sv.trainName);
                 var destinationData = $("<td>");
                 destinationData.text(sv.destination);
-                var dateData = $("<td>");
-                dateData.text(sv.date);
                 var frequencyData = $("<td>");
                 frequencyData.text(sv.frequency);
+
+                var firstTime = sv.firstTrain;
+                var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+                var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+                var tRemainder = diffTime % sv.frequency;
+                var tMinutesTillTrain = sv.frequency - tRemainder;
+                var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+                //create new td for 1) next train time +add this text: moment(nextTrain).format("hh:mm") 
+                var nextTrainData = $("<td>");
+                nextTrainData.text(moment(nextTrain).format("hh:mm"));
+                var tMinutesTillTrainData = $("<td>");
+                tMinutesTillTrainData.text(tMinutesTillTrain);
+                //and 2) minutes until the arrival
+
                 // Console.loging the last user's data
-                console.log(sv.name);
+                console.log(sv.trainName);
                 console.log(sv.destination);
-                console.log(sv.date);
+                console.log(sv.firstTrain);
                 console.log(sv.frequency);
-                newRow.append(nameData, destinationData, dateData, frequencyData);
+                newRow.append(nameData, destinationData, frequencyData, nextTrainData, tMinutesTillTrainData);
                 $("tbody").append(newRow);
             });
 });
